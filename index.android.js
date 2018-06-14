6c346791ebd02deebb94fede77feb85ee9b0de4c
Piotr Zientara <314zientara@gmail.com>
2018-06-14 14:49:30 +0200

    wip

--- index.android.js
+++ index.android.js
@@ -1,4 +1,6 @@
 import { AppRegistry } from 'react-native';
+import { document } from 'react-native-web';
 import App from './src/App';
 
 AppRegistry.registerComponent('ReactNativeTypeScriptStarter', () => App);
+AppRegistry.runApplication('App', { rootTag: document.getElementById('react-root') });
