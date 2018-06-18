a9cbd73b0d04dbae011c6f073614c623858399be
Piotr Zientara <314zientara@gmail.com>
2018-06-18 16:58:44 +0200

    Fix: use one file for index.js

--- index.android.js
+++
@@ -1,5 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './src/App';
-
-AppRegistry.registerComponent('ReactNativeTypeScriptStarter', () => App);
-AppRegistry.runApplication('App', { rootTag: document.getElementById('react-root') });
